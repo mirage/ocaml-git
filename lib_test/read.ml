872d81b931a85d1f4e3cedbf0d3facaff50c4f61
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-06 22:59:05 +0100

    Update the test

--- lib_test/read.ml
+++ lib_test/read.ml
@@ -1,6 +1,14 @@
 open GitTypes
+open Lwt
+
+let main () =
+(*  Log.(set_log_level DEBUG); *)
+  GitLocal.set_auto_flush false;
+  let t = GitLocal.create () in
+  GitLocal.dump t >>= fun () ->
+  GitGraph.to_dot t "graph.dot" >>= fun () ->
+  return_unit
 
 let () =
-  let t = GitLocal.create () in
-  GitLocal.dump t;
-  GitGraph.to_dot t "graph.dot"
+  Log.color_on ();
+  Lwt_unix.run (main ())
