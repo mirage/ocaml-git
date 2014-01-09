6327f4b47b33303b45d25f48558740f5b6c38c03
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-01-09 15:53:44 +0100

    Fix some doc messages

--- lib/git.mli
+++ lib/git.mli
@@ -112,16 +112,13 @@ end
 
 val mem: succ:(sha1 -> successor list Lwt.t) -> sha1 -> string list -> bool Lwt.t
 (** [mem t sha1 path] check wether we can go from the object named
-    [sha1] to an other object following the [path] labels. This
-    could work for tree and tag objects only. *)
+    [sha1] to an other object following the [path] labels. *)
 
 val find: succ:(sha1 -> successor list Lwt.t) -> sha1 -> string list -> sha1 option Lwt.t
 (** [find t sha1 path] returns (if it exists) the object named [sha1]
-    to an other object following the [path] labels. This could work
-    for tree and tag objects only. *)
+    to an other object following the [path] labels. *)
 
 val find_exn: succ:(sha1 -> successor list Lwt.t) -> sha1 -> string list -> sha1 Lwt.t
 (** [find t sha1 path] returns (if it exists) the object named [sha1]
-    to an other object following the [path] labels. This could work
-    for tree and tag objects only. Raise [Not_found] if no such
-    object exist.*)
+    to an other object following the [path] labels. Raise [Not_found]
+    if no such object exist.*)
