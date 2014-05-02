23e3de54858f9b7bf84026af888e5bdde0b9395e
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-05-02 19:21:21 +0100

    Fix travis deps

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 # OPAM packages needed to build tests.
 OPAM_PACKAGES="camlzip dolog core_kernel cryptokit uri \
                cmdliner lazy-trie mstruct re ocamlgraph \
-               alcotest lwt"
+               alcotest lwt patience"
 
 ppa=avsm/ocaml41+opam11
 echo "yes" | sudo add-apt-repository ppa:$ppa
