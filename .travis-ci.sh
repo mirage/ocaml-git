17dcb9a4ec0fd4b999032474b164f449a50737a6
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-06-09 19:49:55 +0100

    Update Travis file

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 # OPAM packages needed to build tests.
 OPAM_PACKAGES="camlzip dolog core_kernel cryptokit uri \
-               cmdliner lazy-trie mstruct re ocamlgraph \
-               alcotest lwt conduit uri"
+               cmdliner sha mstruct re ocamlgraph \
+               alcotest lwt conduit uri mirage-fs-unix"
 
 ppa=avsm/ocaml41+opam11
 echo "yes" | sudo add-apt-repository ppa:$ppa
@@ -10,9 +10,10 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
 
 opam init https://github.com/ocaml/opam-repository.git
+opam pin mirage-fs-unix https://github.com/samoht/mirage-fs-unix.git
 opam install ${OPAM_PACKAGES}
 
 eval `opam config env`
-ocaml setup.ml -configure --enable-tests
+ocaml setup.ml -configure --enable-tests --enable-mirage
 make
 make test
