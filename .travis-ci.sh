d272ef56daec87b9ca2aca81e2fb5e177cf8fbba
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-10-16 19:23:57 +0100

    Add the correct external deps in Travis scripts for zarith

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,7 +7,7 @@ OPAM_PACKAGES="camlzip dolog nocrypto uri \
 ppa=avsm/ocaml41+opam11
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam libgmp-dev
 export OPAMYES=1
 
 opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
