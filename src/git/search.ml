0e2442c95dbede6daddc304e1bcd725d71b81df7
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-09-14 06:45:41 -0700

    Add forgotten credits (Romain Calascibetta) in the header

--- src/git/search.ml
+++ src/git/search.ml
@@ -1,5 +1,6 @@
 (*
  * Copyright (c) 2013-2017 Thomas Gazagnaire <thomas@gazagnaire.org>
+ * and Romain Calascibetta <romain.calascibetta@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
