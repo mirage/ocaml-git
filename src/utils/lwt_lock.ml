8f3ef5e3f25610281eee1b76d941c21ac70db1d8
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-09-27 02:56:32 -0700

    Add license about the implementation of Stack and Weaktbl

--- src/utils/lwt_lock.ml
+++ src/utils/lwt_lock.ml
@@ -1,3 +1,21 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                            Weaktbl                                  *)
+(*                                                                     *)
+(*             (C) 2007 by Zheng Li (li@pps.jussieu.fr)                *)
+(*                                                                     *)
+(*  This program is free software; you can redistribute it and/or      *)
+(*  modify it under the terms of the GNU Lesser General Public         *)
+(*  License version 2.1 as published by the Free Software Foundation,  *)
+(*  with the special exception on linking described in file LICENSE.   *)
+(*                                                                     *)
+(*  This program is distributed in the hope that it will be useful,    *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of     *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *)
+(*  GNU Library General Public License for more details.               *)
+(*                                                                     *)
+(***********************************************************************)
+
 module Stack =
 struct
   type 'a t =
