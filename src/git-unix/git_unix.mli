ef0be63c733439221a69226691eaa031864535d7
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-10-02 08:41:31 -0700

    Add the git-unix implementation

--- src/git-unix/git_unix.mli
+++ src/git-unix/git_unix.mli
@@ -16,63 +16,8 @@
 
 (** Unix backend. *)
 
-open Git
-
-module type S = sig
-
-  (** {1 Unix implementation}
-
-      The default implementation for Unix fixes the choice of
-      [Git.Inflate.S] and [Git.Hash.DIGEST] by using [camlzip]'s zlib
-      bindings and [nocrypto]'s SHA1 and SHA256 implementations.
-
-      It also uses [Lwt_unix] and [Lwt_io] for the IO functions. *)
-
-  (** {1 Synchronisation} *)
-
-  module Sync: sig
-    module IO: Sync.IO
-    module Result: (module type of Sync.Result
-                     with type fetch = Sync.Result.fetch
-                      and type push  = Sync.Result.push)
-    module Make (S: Store.S): Sync.S with type t = S.t
-  end
-
-  (** {1 Filesystem} *)
-
-  (** Implementation of the on-disk Git protocol using [Lwt_unix]. *)
-  module FS: sig
-    module IO: FS.IO
-    include FS.S
-  end
-
-  module Mem: Store.S
-  (** In-memory store. *)
-
-  (** {1 Git objects} *)
-
-  module Hash_IO: Hash.IO
-  module Value_IO: Value.IO
-  module Pack_IO: Pack.IO
-  module Index_IO: Index.IO
-
-end
-
-include S
-
-module Make (D: Git.Hash.DIGEST) (I: Git.Inflate.S): S
-(** Parameterize the Git implementation with different inflate and hash
-    algorithms. {b Note:} this might cause your implementation to not
-    be compatible with Git anymore! *)
-
-module Zlib: Inflate.S
-(** Implementation of the inflate signature using [camlzip]'s zlib
-    bindings. *)
-
-module SHA1: Git.Hash.DIGEST
-(** Implementation of the digest signature using [ocaml-nocrypto]'s
-    Hash1 algorithm. *)
-
-module SHA256: Git.Hash.DIGEST
-(** Implementation of the digest signature using [ocaml-nocrypto]'s
-    Hash256 algorithm. *)
+module Store : Git.Store.S
+  with module Hash = Sha1
+   and module Path = Fpath
+   and module Lock = Fs_lwt_unix.Lock
+   and module FileSystem = Fs_lwt_unix.Fs
