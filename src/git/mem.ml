b4d8c6d692b266061e7ea64cdccbd1165b13c660
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-10-11 05:47:05 -0700

    Add Pack.from implementation on the memory back-end

--- src/git/mem.ml
+++ src/git/mem.ml
@@ -180,6 +180,223 @@ module Make
     Lwt_list.map_s (fun h -> read_exn t h >|= fun value -> h, value) hashes >|= fun values ->
     (Ok values : ((Hash.t * Value.t) list, error) result)
 
+  module Pack =
+  struct
+    type stream = unit -> Cstruct.t option Lwt.t
+
+    module PACKDecoder = Unpack.MakePACKDecoder(Hash)(Inflate)
+    module Revidx = Map.Make(Int64)
+
+    type error =
+      [ `Unresolved_object
+      | `DecoderFlow of string
+      | `PackDecoder of PACKDecoder.error ]
+
+    let pp_error ppf = function
+      | `Unresolved_object -> Fmt.pf ppf "`Unresolved_object"
+      | `DecoderFlow s -> Fmt.pf ppf "(`PackFlow %s)" s
+      | `PackDecoder e -> Fmt.pf ppf "(`PackDecoder %a)" PACKDecoder.pp_error e
+
+    let make _ ?window:(_= `Object 10) ?depth:(_= 50) _ = assert false
+
+    let option_map f v = match v with Some v -> Some (f v) | None -> None
+    let option_map_default f d v = match v with Some v -> f v | None -> d
+    let option_default v = function Some v -> v | None -> v
+
+    type optimized_hunk =
+      | Insert of (int * int)
+      | Copy of (int * int)
+
+    let pp_optimized_hunk ppf = function
+      | Insert (off, len) ->
+        Fmt.pf ppf "(Insert { @[<hov>length = %d;@ \
+                    offset = %d;@] })"
+          len off
+      | Copy (off, len) ->
+        Fmt.pf ppf "(Copy { @[<hov>length = %d;@ \
+                    offset = %d;@] })"
+          len off
+
+    let from git stream =
+      let cstruct_copy cs =
+        let ln = Cstruct.len cs in
+        let rs = Cstruct.create ln in
+
+        Cstruct.blit cs 0 rs 0 ln;
+        rs
+      in
+
+      let apply hunks_descr hunks buffer_hunks source target =
+        if Cstruct.len target < hunks_descr.PACKDecoder.H.target_length
+        then raise (Invalid_argument "apply");
+
+        let target_length =
+          List.fold_left
+            (fun acc -> function
+               | Insert (off, len) ->
+                 Cstruct.blit buffer_hunks off target acc len; acc + len
+               | Copy (off, len) ->
+                 Cstruct.blit source off target acc len; acc + len)
+            0 hunks
+        in
+
+        if target_length = hunks_descr.PACKDecoder.H.target_length
+        then Ok (Cstruct.sub target 0 target_length)
+        else Error (Fmt.strf "Bad undelta-ification (result: %d, expect: %d)" target_length hunks_descr.PACKDecoder.H.target_length)
+      in
+
+      let k2k = function
+        | PACKDecoder.Commit -> `Commit
+        | PACKDecoder.Tag -> `Tag
+        | PACKDecoder.Tree -> `Tree
+        | PACKDecoder.Blob -> `Blob
+        | PACKDecoder.Hunk _ -> raise (Invalid_argument "k2k")
+      in
+
+      let open Lwt.Infix in
+
+      let empty        = Cstruct.create 0 in
+      let buffer       = Buffer.create 0x800 in
+      let buffer_hunks = Buffer.create 0x800 in
+      let queue        = Queue.create () in
+
+      let rec go ~revidx ?(src = empty) ?hunks state =
+        match PACKDecoder.eval src state with
+        | `Await state ->
+          (stream () >>= function
+            | Some raw ->
+              go ~revidx ~src:raw ?hunks (PACKDecoder.refill 0 (Cstruct.len raw) state)
+            | None ->
+              Lwt.return (Error (`DecoderFlow "Unexpected end of stream")))
+        | `End (state, hash_pack) ->
+          Lwt.return (Ok (hash_pack, PACKDecoder.many state))
+        | `Error (_, err) ->
+          Log.err (fun l -> l ~header:"populate" "The PACK decoder returns an error: %a." PACKDecoder.pp_error err);
+          Lwt.return (Error (`PackDecoder err))
+        | `Flush state ->
+          let o, n = PACKDecoder.output state in
+
+          Buffer.add buffer (Cstruct.sub o 0 n);
+          go ~revidx ~src (PACKDecoder.flush 0 (Cstruct.len o) state)
+        | `Hunk (state, hunk) ->
+          let hunks = match hunks, hunk with
+            | Some hunks, PACKDecoder.H.Insert raw ->
+              let off = Buffer.has buffer_hunks in
+              Buffer.add buffer_hunks raw;
+
+              (Insert (off, Cstruct.len raw) :: hunks)
+            | Some hunks, PACKDecoder.H.Copy (off, len) ->
+              (Copy (off, len) :: hunks)
+            | None, PACKDecoder.H.Insert raw ->
+              let off = Buffer.has buffer_hunks in
+              Buffer.add buffer_hunks raw;
+
+              [ Insert (off, Cstruct.len raw) ]
+            | None, PACKDecoder.H.Copy (off, len) ->
+              [ Copy (off, len) ]
+          in
+
+          go ~revidx ~src ~hunks (PACKDecoder.continue state)
+        | `Object state ->
+          (match PACKDecoder.kind state with
+           | (PACKDecoder.Commit
+             | PACKDecoder.Tag
+             | PACKDecoder.Tree
+             | PACKDecoder.Blob) as kind ->
+
+             let raw = Buffer.contents buffer |> Cstruct.of_string in
+             Buffer.clear buffer;
+
+             Log.debug (fun l -> l ~header:"populate" "Retrieve a new Git object (length: %d)." (Cstruct.len raw));
+
+             write_inflated git ~kind:(k2k kind) raw >|= fun hash ->
+             Log.debug (fun l -> l ~header:"populate" "Add the object %a to the Git repository from the PACK file."
+                           Hash.pp hash);
+             Some hash
+           | PACKDecoder.Hunk hunks_descr ->
+             let hunks = option_map List.rev hunks |> option_default [] in
+             let inflated = Cstruct.create (hunks_descr.PACKDecoder.H.target_length) in
+             let hash_source = match hunks_descr.PACKDecoder.H.reference with
+               | PACKDecoder.H.Hash hash -> hash
+               | PACKDecoder.H.Offset off ->
+                 let off = Int64.sub (PACKDecoder.offset state) off in
+                 Revidx.find off revidx
+
+             (* XXX(dinosaure): This is come from an assumption about
+                the PACK file. Any hunk object has source as an external
+                Git object of the PACK file or a previous Git object.
+
+                So [Revidx.find] should never fail. *)
+             in
+
+             Log.debug (fun l -> l ~header:"populate" "Catch a Hunk object which has as source the Git object: %a."
+                           Hash.pp hash_source);
+
+             read_inflated git hash_source >>= function
+             | None ->
+               Log.warn (fun l -> l ~header:"populate" "The source Git object %a does not exist yet." Hash.pp hash_source);
+               Queue.push (hunks_descr, hunks, cstruct_copy (Buffer.unsafe_contents buffer_hunks), inflated, hash_source) queue;
+               Buffer.clear buffer_hunks;
+
+               Lwt.return None
+             | Some (kind, raw) ->
+               Log.debug (fun l -> l ~header:"populate" "Retrieving the source Git object %a." Hash.pp hash_source);
+
+               match apply hunks_descr hunks (Buffer.unsafe_contents buffer_hunks) raw inflated with
+               | Ok result ->
+                 Buffer.clear buffer_hunks;
+                 write_inflated git ~kind result >|= fun hash ->
+                 Log.debug (fun l -> l ~header:"populate" "Add the object %a to the Git repository from the PACK file."
+                               Hash.pp hash);
+                 Some hash
+               | Error err ->
+                 Log.err (fun l -> l ~header:"populate" "Error when we apply the source Git object %a with the Hunk object: %a."
+                             Hash.pp hash_source
+                             (Fmt.hvbox (Fmt.list ~sep:(Fmt.const Fmt.string ";@ ") pp_optimized_hunk)) hunks);
+                 Lwt.fail (Failure err))
+          >>= fun hash ->
+          let revidx = option_map_default (fun hash -> Revidx.add (PACKDecoder.offset state) hash revidx) revidx hash in
+          go ~revidx ~src (PACKDecoder.next_object state)
+      in
+
+      let rec gogo () = match Queue.pop queue with
+        | exception Queue.Empty -> Lwt.return (Ok ())
+        | (hunks_descr, hunks, buffer_hunks, inflated, hash_source) ->
+          read_inflated git hash_source >>= function
+          | None ->
+            Queue.push (hunks_descr, hunks, buffer_hunks, inflated, hash_source) queue;
+            gogo ()
+          | Some (kind, raw) ->
+            match apply hunks_descr hunks buffer_hunks raw inflated with
+            | Ok result -> write_inflated git ~kind result >>= fun _ -> gogo ()
+            | Error err -> Lwt.fail (Failure err)
+      in
+
+      let ztmp = Cstruct.create 0x800 in
+      let window = PACKDecoder.Inflate.window () in
+
+      go ~revidx:Revidx.empty (PACKDecoder.default ztmp window)
+      >>= function
+      | Error _ as err -> Lwt.return err
+      | Ok (hash_pack, n) ->
+        let n = Int32.to_int n in
+
+        Queue.fold (fun acc x -> x :: acc) [] queue
+        |> Lwt_list.fold_left_s (fun acc (_, _, _, _, hash) -> exists git hash >|= function
+          | true -> acc + 1
+          | false -> acc) 0
+        >>= function
+        | 0 ->
+          if Queue.is_empty queue
+          then Lwt.return (Ok (hash_pack, n))
+          else Lwt.return (Error `Unresolved_object)
+        | _ ->
+          let open Lwt_result in
+
+          Log.debug (fun l -> l ~header:"populate" "Resolving the rest of the PACK file.");
+          gogo () >>= fun () -> Lwt.return (Ok (hash_pack, n))
+  end
+
   module Ref =
   struct
     type error = [ `Not_found ]
