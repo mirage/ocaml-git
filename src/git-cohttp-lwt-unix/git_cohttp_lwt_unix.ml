da54faa6f299805b36cebaa88d8648a820ebc614
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-11-02 02:47:17 -0700

    Add a specialized version of git-http with cohttp-lwt-unix
    Indeed, git-http was specialized with cohttp-lwt but not
    cohttp-lwt-unix. That means, the user can user git-http with cohttp-js
    for example.

---
+++ src/git-cohttp-lwt-unix/git_cohttp_lwt_unix.ml
@@ -0,0 +1,85 @@
+module CohttpClient =
+struct
+  type headers = Cohttp.Header.t
+  type resp = Git_http.Web_cohttp_lwt.resp =
+    { resp : Cohttp.Response.t
+    ; body : Cohttp_lwt.Body.t }
+  type body = unit -> (Cstruct.t * int * int) option Lwt.t
+  type meth = Cohttp.Code.meth
+  type uri = Uri.t
+
+  type +'a io = 'a Lwt.t
+
+  module Log =
+  struct
+    let src = Logs.Src.create "cohttp" ~doc:"logs cohttp event"
+    include (val Logs.src_log src : Logs.LOG)
+  end
+
+  let call ?headers ?body meth uri =
+    let open Lwt.Infix in
+
+    let body = match body with
+      | None -> None
+      | Some stream ->
+        Lwt_stream.from stream
+        |> Lwt_stream.map (fun (buf, off, len) -> Cstruct.to_string (Cstruct.sub buf off len))
+        |> fun stream -> Some (`Stream stream)
+    in
+
+    (* XXX(dinosaure): [~chunked:true] is mandatory, I don't want to
+       explain why (I lost one day to find this bug) but believe me. *)
+    Cohttp_lwt_unix.Client.call ?headers ?body ~chunked:false meth uri >>= fun ((resp, _) as v) ->
+    if Cohttp.Code.is_redirection (Cohttp.Code.code_of_status (Cohttp.Response.status resp))
+    then begin
+      let uri =
+        Cohttp.Response.headers resp
+        |> Cohttp.Header.to_list
+        |> List.assoc "location"
+        |> Uri.of_string
+      in
+
+      Log.info (fun l -> l ~header:"call" "Redirection to %a." Uri.pp_hum uri);
+
+      Cohttp_lwt_unix.Client.call ?headers ?body ~chunked:false meth uri >>= fun (resp, body) ->
+      Lwt.return { resp; body; }
+    end else Lwt.return { resp; body = snd v; }
+end
+
+module Make
+    (K : Git.Sync.CAPABILITIES)
+    (S : Git.Minimal.S with type Hash.Digest.buffer = Cstruct.t
+                        and type Hash.hex = string)
+= struct
+  include Git_http.Make(K)(CohttpClient)(S)
+  module Negociator = Git.Negociator.Make(S)
+
+  let fetch_all t repository =
+    let open Lwt.Infix in
+
+    Negociator.find_common t >>= fun (has, state, continue) ->
+    let continue { Decoder.acks; shallow; unshallow } state =
+      continue { Git.Negociator.acks; shallow; unshallow } state
+    in
+
+    let want refs =
+      Lwt_list.filter_map_p
+        (function (hash, refname, false) ->
+           let reference = S.Reference.of_string refname in
+           Lwt.return (Some (reference, hash))
+           (* XXX(dinosaure): we consider than remote reference was binded with local reference (which has the same name). *)
+                | _ -> Lwt.return None)
+        refs
+    in
+    let host = match Uri.host repository with
+      | Some host -> host
+      | None -> raise (Invalid_argument (Fmt.strf "Expected an http url with host: %a." Uri.pp_hum repository))
+    in
+    let https = match Uri.scheme repository with
+      | Some "https" -> true
+      | _ -> false
+    in
+
+    fetch t ~https ?port:(Uri.port repository) ~negociate:(continue, state) ~has ~want host (Uri.path_and_query repository)
+
+end
---
+++ src/git-cohttp-lwt-unix/git_cohttp_lwt_unix.mli
@@ -0,0 +1,26 @@
+module CohttpClient
+  : Git.Sync_http.CLIENT
+    with type +'a io  = 'a Lwt.t
+     and type headers = Git_http.Web_cohttp_lwt.HTTP.headers
+     and type body    = unit -> (Cstruct.t * int * int) option Lwt.t
+     and type meth    = Git_http.Web_cohttp_lwt.HTTP.meth
+     and type uri     = Git_http.Web_cohttp_lwt.uri
+     and type resp    = Git_http.Web_cohttp_lwt.resp
+
+module Make
+    (K : Git.Sync.CAPABILITIES)
+    (S : Git.Minimal.S with type Hash.Digest.buffer = Cstruct.t
+                        and type Hash.hex = string)
+: sig
+  module Negociator
+    : Git.Negociator.S
+      with module Store := S
+
+  include Git.Sync_http.S
+    with module Web    := Git_http.Web_cohttp_lwt
+     and module Client := CohttpClient
+     and module Store  := S
+     and module Buffer := Git_http.Cstruct_buffer
+
+  val fetch_all : S.t -> Uri.t -> ((S.Reference.t * S.Hash.t) list * int, error) result Lwt.t
+end
