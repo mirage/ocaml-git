8ceeaa425be9d2ea2023fdbfe51aa7b2249192d8
Romain Calascibetta <romain.calascibetta@gmail.com>
2017-11-25 01:39:01 -0800

    Add an easy interface to use the Smart implementation on the TCP way (with unix)

---
+++ src/git-tcp-unix/git_tcp_unix.mli
@@ -0,0 +1,27 @@
+module Default :
+sig
+  val default : Git.Capability.t list
+end
+
+module Make
+    (K : Git.Sync.CAPABILITIES)
+    (S : Git.Minimal.S
+     with type Hash.Digest.buffer = Cstruct.t
+      and type Hash.hex = string)
+: sig
+  module Negociator
+    : Git.Negociator.S
+      with module Store := S
+
+  include Git.Sync.S
+    with module Store := S
+
+  type error' =
+    [ `StoreRef of S.Ref.error
+    | `Sync of error ]
+
+  val fetch_all   : S.t -> ?locks:S.Lock.t -> Uri.t -> (unit, error') result Lwt.t
+  val fetch_one   : S.t -> ?locks:S.Lock.t -> reference:S.Reference.t -> Uri.t -> (unit, error') result Lwt.t
+  val easy_clone  : S.t -> ?locks:S.Lock.t -> reference:S.Reference.t -> Uri.t -> (unit, error') result Lwt.t
+  val easy_update : S.t -> reference:S.Reference.t -> Uri.t -> ((S.Reference.t, S.Reference.t * string) result list, error') result Lwt.t
+end
