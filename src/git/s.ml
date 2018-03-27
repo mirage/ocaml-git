87fdb638f5c56cfa4fce8a15252e2d5c6d7d626a
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-03-27 18:55:27 +0200

    Delete useless FDIGEST intf

--- src/git/s.ml
+++ src/git/s.ml
@@ -107,21 +107,6 @@ module type DEFLATE = sig
   val eval       : src:Cstruct.t -> dst:Cstruct.t -> t -> [ `Flush of t | `Await of t | `Error of (t * error) | `End of t ]
 end
 
-module type FDIGEST = sig
-  type t
-  type buffer
-
-  type 'a digest = 'a -> t
-
-  val cstruct : Cstruct.t digest
-  val string  : string digest
-  val bytes   : Bytes.t digest
-  val digest  : buffer digest
-  val digestv : buffer list digest
-
-  val length  : int
-end
-
 module type IDIGEST = sig
   type t
   type ctx
