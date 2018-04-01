464e971a58ea45bb9c45a88bcd09ed94c397ab9c
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-04-01 20:28:35 +0200

    Revert path of PACK and IDX file available to test them om test_data.ml

--- test/common/test_data.ml
+++ test/common/test_data.ml
@@ -162,8 +162,7 @@ module Make0 (Source: SOURCE) (Store: S) = struct
         | Ok v -> Lwt.return v
         | Error err -> Alcotest.failf "Got an error: %a." pp_error err)
 
-  let pwd = Unix.getcwd ()
-  let root = Fpath.(v pwd / "test-data" / Source.name)
+  let root = Fpath.(v "test-data" / Source.name)
 
   let verify_unpack () = import root () >?= fun _ -> Lwt.return (Ok ())
 
