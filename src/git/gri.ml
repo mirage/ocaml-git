37646c2b8aadf04d8dac67c0c648aaafb816c6d1
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-09-13 15:35:05 +0200

    Add the GRI module (URI with git scheme)

---
+++ src/git/gri.ml
@@ -0,0 +1,19 @@
+type t = Uri.t
+
+let host uri =
+  match Uri.host uri with
+  | Some host -> host
+  | None -> Fmt.invalid_arg "Invalid gri: no host"
+
+let path uri = Uri.path_and_query uri
+let pp ppf x = Fmt.string ppf (Uri.to_string x)
+
+let make : Uri.t -> t =
+ fun uri ->
+  match Uri.scheme uri, Uri.host uri with
+  | Some "git", Some _ -> uri
+  | Some scheme, _ -> Fmt.invalid_arg "Invalid gri: invalid scheme (%s)" scheme
+  | _, None -> Fmt.invalid_arg "Invalid gri: no host"
+  | None, Some _ -> Fmt.invalid_arg "Invalid gri: no scheme"
+
+let of_string x = make (Uri.of_string x)
