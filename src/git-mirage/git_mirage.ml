679925f5456f3583793f624af0db417211bebf4d
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-10-06 19:00:53 +0100

    Remove Git_mirage.Store
    That's not the way we want to support Git backend with Mirage: there's
    too many POSIX assumptions baked in that model...

--- src/git-mirage/git_mirage.ml
+++ src/git-mirage/git_mirage.ml
@@ -1,17 +1,4 @@
 module Net = Net
-module FS = Fs
-
-module Store (X : Mirage_fs_lwt.S) = struct
-  module F = Fs.Make (X)
-  module I = Git.Inflate
-  module D = Git.Deflate
-  module S = Git.Store.Make (Digestif.SHA1) (F) (I) (D)
-  include S
-
-  let v ?current_dir ?dotgit ?compression ?buffer fs root =
-    let fs = F.v ?current_dir fs in
-    v ?dotgit ?compression ?buffer fs root
-end
 
 type endpoint = Net.endpoint = {
   uri     : Uri.t;
