e42f5392ad7a25e18d3327a7fea15a6134ed62fa
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-08-20 11:10:24 +0200

    Documentation on Commit module

--- src/git/commit.mli
+++ src/git/commit.mli
@@ -15,8 +15,16 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-(** A Git Commit object. *)
+(** A Git Commit object.
+
+    A Git object which contains the information about a particular revision,
+    such as parents, {i committed}, author, date and {!Tree.t} which corresponds
+    to the top directory of the stored revision. *)
+
 type 'hash t
+(** A Git Commit object. Which specifies the top-level {!Tree.t} for the
+    snapshot of the project at a point; the author/{i committer} information and
+    the commit message. *)
 
 module type S = sig
   type hash
@@ -33,43 +41,43 @@ module type S = sig
     t
   (** [make ~author ~committer ?parents ~tree msg] makes an OCaml value {!t}.
       [?parents] should be a non-empty list and corresponds to a list of hashes
-      of commits. [tree] should be a hash of a {Tree.t} object.
+      of parent commits. [tree] should be a hash of a {!Tree.t} object.
 
-      This function does not write a new commit on the store and does not check
-      the validity of [parents] and [tree]. By this way, this function never
-      fails. *)
-
+      {b Note.} This function does not write a new commit on the store and does
+      not check the validity of [parents] and [tree]. By this way, this function
+      never fails. *)
 
   val format : t Encore.t
+  (** [format] is a description of how to encode/decode of {!t} object. *)
 
   include S.DIGEST with type t := t and type hash := hash
 
   include S.BASE with type t := t
 
   val length : t -> int64
-  (** [length t] returns the length of the commit object [t]. *)
+  (** [length t] returns the length of the given commit object. *)
 
-  (** [parents c] returns all parents of the Git Commit object [c]. *)
   val parents : t -> hash list
+  (** [parents t] returns all parents of the given commit object. *)
 
-  (** [tree c] returns the hash of top-level {!Tree.t} of the Git Commit object
-      [c]. *)
   val tree : t -> hash
+  (** [tree t] returns the hash of top-level {!Tree.t} of the given commit
+      object. *)
 
   val committer : t -> User.t
-  (** [committer c] returns the committer of the commit [c]. *)
+  (** [committer t] returns the committer of the given commit object. *)
 
   val author : t -> User.t
-  (** [author c] returns the author of the commit [c]. *)
+  (** [author c] returns the author of the given commit object. *)
 
   val message : t -> string
-  (** [message c] returns the message of the commit [c]. *)
+  (** [message c] returns the message of the given commit object. *)
 
   val extra : t -> (string * string list) list
 
   val compare_by_date : t -> t -> int
-  (** [compare_by_date a b] compares the Git Commit object [a] and [b] by the
-      date of the author. The {!compare} function as the same behaviour. *)
+  (** [compare_by_date a b] compares commit objects [a] and [b] by the date of
+      the author. The {!compare} function as the same behaviour. *)
 end
 
 (** The {i functor} to make the OCaml representation of the Git Commit object by
