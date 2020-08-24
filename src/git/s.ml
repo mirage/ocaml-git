2331adc1d016a75b941df3eae9d36d7d21a5e970
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-08-24 18:15:01 +0200

    Documentation on S module

--- src/git/s.ml
+++ src/git/s.ml
@@ -19,12 +19,19 @@ module type BASE = sig
   type t
 
   val pp : t Fmt.t
+  (** Pretty-printer of {!t}. *)
 
   val compare : t -> t -> int
+  (** The comparison function for {!t}. *)
 
   val hash : t -> int
+  (** [hash blob] associates a non-negative integer to any value of {!t}. It is
+      guaranteed that if [x = y] or [compare x y = 0], then [hash x = hash y]. *)
 
   val equal : t -> t -> bool
+  (** The equal function for {!t}. *)
+
+  (** {2 Sets and Maps.} *)
 
   module Set : Set.S with type elt = t
 
@@ -69,6 +76,9 @@ module type DIGEST = sig
   type t
 
   type hash
+  (** Type of digests. *)
 
   val digest : t -> hash
+  (** [digest t] associates a [hash] to any value of {!t}. It is guaranteed that
+      if [x = y] or [compare x y = 0], then [digest x = digest y]. *)
 end
