6291e395c6db97065386bdd4818377313903f3ff
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-08-26 11:24:00 +0200

    Add [carton-git] to provide an impl. which needs an append-only store
    As the logic of Git, _loose_ spaces and PACK files, we are able to abstract
    the space where PACK files are localised as an append-only store. This impl.
    depends on [carton-lwt] to directly use the LWT scheduler.

---
+++ src/carton-git/carton_git.mli
@@ -0,0 +1,83 @@
+module type STORE = sig
+  type 'a rd = < rd : unit ; .. > as 'a
+
+  type 'a wr = < wr : unit ; .. > as 'a
+
+  type 'a mode =
+    | Rd : < rd : unit > mode
+    | Wr : < wr : unit > mode
+    | RdWr : < rd : unit ; wr : unit > mode
+
+  type t
+
+  type uid
+
+  type 'a fd
+
+  type error
+
+  type +'a fiber
+
+  val pp_error : error Fmt.t
+
+  val create : mode:'a mode -> t -> uid -> ('a fd, error) result fiber
+
+  val map : t -> 'm rd fd -> pos:int64 -> int -> Bigstringaf.t fiber
+
+  val close : t -> 'm fd -> (unit, error) result fiber
+
+  val list : t -> uid list fiber
+
+  val length : 'm fd -> int64 fiber
+end
+
+module type IO = sig
+  type +'a t
+
+  val bind : 'a t -> ('a -> 'b t) -> 'b t
+
+  val return : 'a -> 'a t
+end
+
+type ('path, 'fd, 'uid) t
+
+type 'fd buffers = {
+  z : Bigstringaf.t;
+  allocate : int -> De.window;
+  w : 'fd Carton.Dec.W.t;
+}
+
+module Make
+    (Scheduler : Carton.SCHEDULER)
+    (IO : IO with type +'a t = 'a Scheduler.s)
+    (Store : STORE with type +'a fiber = 'a Scheduler.s)
+    (Uid : Carton.UID) : sig
+  val make :
+    Store.t ->
+    uid_of_major_uid:(Store.uid -> 'uid) ->
+    idx_major_uid_of_uid:(Store.t -> 'uid -> Store.uid) ->
+    (Store.uid, < rd : unit > Store.fd, Uid.t) t IO.t
+
+  val add :
+    Store.t ->
+    (Store.uid, < rd : unit > Store.fd, Uid.t) t ->
+    idx:Store.uid ->
+    Store.uid ->
+    (unit, Store.error) result IO.t
+
+  val get :
+    Store.t ->
+    resources:
+      (< rd : unit > Store.fd * int64 ->
+      ((< rd : unit > Store.fd * int64) buffers -> Carton.Dec.v IO.t) ->
+      Carton.Dec.v IO.t) ->
+    (Store.uid, < rd : unit > Store.fd, Uid.t) t ->
+    Uid.t ->
+    (Carton.Dec.v, [> `Msg of string | `Not_found of Uid.t ]) result IO.t
+
+  val exists : Store.t -> (Store.uid, 'm Store.fd, Uid.t) t -> Uid.t -> bool
+
+  val list : Store.t -> (Store.uid, 'm Store.fd, Uid.t) t -> Uid.t list
+
+  val fds : (Store.uid, 'm Store.fd, Uid.t) t -> ('m Store.fd * int64) list
+end
