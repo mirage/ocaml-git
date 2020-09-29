b668a43d8c17fa32a86860a0b3a744d7fae336dd
Hannes Mehnert <hannes@mehnert.org>
2020-09-29 11:26:59 +0200

    adapt to cohttp changes

--- src/git-cohttp/git_cohttp.ml
+++ src/git-cohttp/git_cohttp.ml
@@ -1,7 +1,7 @@
 open Lwt.Infix
 
 module Make
-    (Cohttp_client : Cohttp_lwt.S.Client with type resolvers = Conduit.resolvers) =
+    (Cohttp_client : Cohttp_lwt.S.Client with type ctx = Conduit.resolvers) =
 struct
   type error = |
 
@@ -9,13 +9,13 @@ struct
 
   let get ~resolvers ?(headers = []) uri =
     let headers = Cohttp.Header.of_list headers in
-    Cohttp_client.get ~resolvers ~headers uri >>= fun (_response, body) ->
+    Cohttp_client.get ~ctx:resolvers ~headers uri >>= fun (_response, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body -> Lwt.return_ok ((), body)
 
   let post ~resolvers ?(headers = []) uri body =
     let headers = Cohttp.Header.of_list headers in
     let body = Cohttp_lwt.Body.of_string body in
-    Cohttp_client.post ~resolvers ~headers ~chunked:false ~body uri
+    Cohttp_client.post ~ctx:resolvers ~headers ~chunked:false ~body uri
     >>= fun (_response, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body -> Lwt.return_ok ((), body)
 end
