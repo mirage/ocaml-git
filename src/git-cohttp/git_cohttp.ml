7c719edde932d8afd2c7cafa06571a32c499da72
dinosaure <romain.calascibetta@gmail.com>
2020-12-20 00:16:58 +0100

    Slight update on git-cohttp (use cohttp.2.5.4)

--- src/git-cohttp/git_cohttp.ml
+++ src/git-cohttp/git_cohttp.ml
@@ -1,21 +1,19 @@
 open Lwt.Infix
 
-module Make
-    (Cohttp_client : Cohttp_lwt.S.Client with type ctx = Conduit.resolvers) =
-struct
+module Make (Cohttp_client : Cohttp_lwt.S.Client) = struct
   type error = |
 
   let pp_error : error Fmt.t = fun _ppf -> function _ -> .
 
-  let get ~resolvers ?(headers = []) uri =
+  let get ?(headers = []) uri =
     let headers = Cohttp.Header.of_list headers in
-    Cohttp_client.get ~ctx:resolvers ~headers uri >>= fun (_response, body) ->
+    Cohttp_client.get ~headers uri >>= fun (_response, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body -> Lwt.return_ok ((), body)
 
-  let post ~resolvers ?(headers = []) uri body =
+  let post ?(headers = []) uri body =
     let headers = Cohttp.Header.of_list headers in
     let body = Cohttp_lwt.Body.of_string body in
-    Cohttp_client.post ~ctx:resolvers ~headers ~chunked:false ~body uri
+    Cohttp_client.post ~headers ~chunked:false ~body uri
     >>= fun (_response, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body -> Lwt.return_ok ((), body)
 end
