bde6d4c7307d5114b0165db285fb749b211eef49
dinosaure <romain.calascibetta@gmail.com>
2021-01-04 23:34:57 +0100

    Handle redirections with CoHTTP (only for git-unix)

--- src/git-cohttp/git_cohttp.ml
+++ src/git-cohttp/git_cohttp.ml
@@ -5,15 +5,34 @@ module Make (Cohttp_client : Cohttp_lwt.S.Client) = struct
 
   let pp_error : error Fmt.t = fun _ppf -> function _ -> .
 
-  let get ?(headers = []) uri =
+  let with_redirects ?(max = 10) ~f uri =
+    if max < 10 then invalid_arg "with_redirects";
+    let tbl = Hashtbl.create 0x10 in
+    let rec go max uri =
+      f uri >>= fun (resp, body) ->
+      let status_code = Cohttp.(Response.status resp |> Code.code_of_status) in
+      if Cohttp.Code.is_redirection status_code then
+        match Cohttp.(Response.headers resp |> Header.get_location) with
+        | Some uri' when Hashtbl.mem tbl uri' || max = 0 ->
+            Lwt.return (resp, body)
+        | Some uri' ->
+            Hashtbl.add tbl uri' ();
+            Cohttp_lwt.Body.drain_body body >>= fun () -> go (pred max) uri'
+        | None -> Lwt.return (resp, body)
+      else Lwt.return (resp, body)
+    in
+    go max uri
+
+  let get ~ctx:_ ?(headers = []) uri =
     let headers = Cohttp.Header.of_list headers in
-    Cohttp_client.get ~headers uri >>= fun (_response, body) ->
+    let f uri = Cohttp_client.get ~headers uri in
+    with_redirects ~f uri >>= fun (_resp, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body -> Lwt.return_ok ((), body)
 
-  let post ?(headers = []) uri body =
+  let post ~ctx:_ ?(headers = []) uri body =
     let headers = Cohttp.Header.of_list headers in
     let body = Cohttp_lwt.Body.of_string body in
-    Cohttp_client.post ~headers ~chunked:false ~body uri
-    >>= fun (_response, body) ->
+    let f uri = Cohttp_client.post ~headers ~chunked:false ~body uri in
+    with_redirects ~f uri >>= fun (_resp, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body -> Lwt.return_ok ((), body)
 end
