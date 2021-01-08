dc16d32cd97484ef7472a1aadf04c0fafa90f66c
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-01-08 15:04:11 +0500

    use _intf design pattern to dedup module types from 'smart_git.ml/mli'

--- src/not-so-smart/smart_git.ml
+++ src/not-so-smart/smart_git.ml
@@ -1,57 +1,10 @@
-module type APPEND = sig
-  type 'a rd = < rd : unit ; .. > as 'a
-  type 'a wr = < wr : unit ; .. > as 'a
-
-  type 'a mode =
-    | Rd : < rd : unit > mode
-    | Wr : < wr : unit > mode
-    | RdWr : < rd : unit ; wr : unit > mode
-
-  type t
-  type uid
-  type 'a fd
-  type error
-  type +'a fiber
-
-  val pp_error : error Fmt.t
-  val create : mode:'a mode -> t -> uid -> ('a fd, error) result fiber
-  val map : t -> 'm rd fd -> pos:int64 -> int -> Bigstringaf.t fiber
-  val append : t -> 'm wr fd -> string -> unit fiber
-  val move : t -> src:uid -> dst:uid -> (unit, error) result fiber
-  val close : t -> 'm fd -> (unit, error) result fiber
-end
-
-module type UID = sig
-  include Carton.UID
-  include Sigs.UID with type t := t
-
-  val hash : t -> int
-end
+include Smart_git_intf
 
 module Verbose = struct
   type 'a fiber = 'a Lwt.t
 
   let succ () = Lwt.return_unit
   let print () = Lwt.return_unit
-end
-
-module type HTTP = sig
-  type error
-
-  val pp_error : error Fmt.t
-
-  val get :
-    ctx:Mimic.ctx ->
-    ?headers:(string * string) list ->
-    Uri.t ->
-    (unit * string, error) result Lwt.t
-
-  val post :
-    ctx:Mimic.ctx ->
-    ?headers:(string * string) list ->
-    Uri.t ->
-    string ->
-    (unit * string, error) result Lwt.t
 end
 
 let ( <.> ) f g x = f (g x)
