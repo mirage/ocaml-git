534ba08a5ee24c9167597b86fae3dbfdf40c9336
Calascibetta Romain <romain.calascibetta@gmail.com>
2021-03-17 10:33:38 +0100

    Keep stability and order on Tree.v and ensure tail-recursivity

--- src/git/tree.ml
+++ src/git/tree.ml
@@ -126,9 +126,8 @@ let value_of_entry = function
 
 let v entries =
   List.rev_map (fun entry -> value_of_entry entry, entry) entries
-  |> List.sort (fun (a, _) (b, _) -> compare a b)
+  |> List.sort (fun (a, _) (b, _) -> compare b a)
   |> List.rev_map snd
-  |> List.rev
 
 let remove ~name t =
   let c = Contents name and n = Node name in
