cf6b82880ca0898b22b878150e58244d99fbb748
Reynir Björnsson <reynir@reynir.dk>
2022-07-29 12:22:19 +0200

    dune build @fmt

--- test/smart/test_edn.ml
+++ test/smart/test_edn.ml
@@ -1,7 +1,6 @@
 let ( <.> ) f g x = f (g x)
 let github_com = "github.com"
 let private_network = "10.0.0.0"
-
 let uri_testable = Alcotest.testable Uri.pp Uri.equal
 
 let test01 =
@@ -90,20 +89,22 @@ let test06 =
 
 let test07 =
   Alcotest.test_case "edn07" `Quick @@ fun () ->
-  let uri = "http://git.data.coop/halfd/new-website.git"  in
+  let uri = "http://git.data.coop/halfd/new-website.git" in
   match Smart_git.Endpoint.of_string uri with
   | Ok
       ({
-        Smart_git.Endpoint.scheme = `HTTP [];
-        hostname = "git.data.coop";
-        port = None;
-        path = "/halfd/new-website.git";
-      } as edn) ->
-    let ctx = Smart_git.Endpoint.to_ctx edn Mimic.empty in
-    Alcotest.(check (option uri_testable)) "uri" (Mimic.get Smart_git.git_uri ctx) (Some (Uri.of_string uri))
+         Smart_git.Endpoint.scheme = `HTTP [];
+         hostname = "git.data.coop";
+         port = None;
+         path = "/halfd/new-website.git";
+       } as edn) ->
+      let ctx = Smart_git.Endpoint.to_ctx edn Mimic.empty in
+      Alcotest.(check (option uri_testable))
+        "uri"
+        (Mimic.get Smart_git.git_uri ctx)
+        (Some (Uri.of_string uri))
   | Ok v -> Alcotest.failf "Unexpeted Git endpoint: %a" Smart_git.Endpoint.pp v
   | Error (`Msg err) -> Alcotest.failf "Unexpected error: %S" err
-
 
 let () =
   Alcotest.run "smart git endpoint"
